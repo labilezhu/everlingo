67d9a5c47a830bc233f1a239a9037a11f68ef89d
labile <labile.zhu@gmail.com>
2026-08-11 23:28:19 +0800

    tag-0.1.2-rc.1

--- src/everlingo/ws_router/app.py
+++ src/everlingo/ws_router/app.py
@@ -127,7 +127,7 @@ class AppState:
 
 def create_app(config: RouterConfig) -> FastAPI:
     state = AppState(config)
-    app = FastAPI(title="WS-Router", version="0.1.1")
+    app = FastAPI(title="WS-Router", version="0.1.2-rc.1")
     app.state.state = state
 
     @app.get("/assets/{path:path}", include_in_schema=False)
